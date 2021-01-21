39d0536bd31e59cd3d8c10deb48e28dff101b317
Dzmitry Dulik <dulikds@gmail.com>
2021-01-21 20:17:23 +0300

    Add JWT for login.

--- users/src/main/java/by/dulik/eureka/users/by/dulik/eureka/users/service/UsersService.java
+++ users/src/main/java/by/dulik/eureka/users/by/dulik/eureka/users/service/UsersService.java
@@ -5,15 +5,20 @@ import by.dulik.eureka.users.by.dulik.eureka.users.dto.UserDto;
 import by.dulik.eureka.users.by.dulik.eureka.users.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Transactional(readOnly = true)
 @Service
-public class UsersService {
+public class UsersService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
@@ -21,8 +26,32 @@ public class UsersService {
     @Transactional
     public UserDto createUser(UserDto newUser) {
         userRepository.save(userMapper.userDtoToUser(newUser));
+        return getUserDtoByEmail(newUser.getEmail());
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return userRepository.findByEmail(username)
+                .map(dto -> User.builder()
+                        .username(dto.getEmail())
+                        .password(dto.getEncryptedPassword())
+                        .disabled(false)
+                        .accountExpired(false)
+                        .credentialsExpired(false)
+                        .accountLocked(false)
+                        .authorities(new ArrayList<>())
+                        .build()
+                )
+                .orElseThrow(() -> new UsernameNotFoundException("Not found user with e-mail : " + username));
+    }
+
+    public UserDto getUserDetailsByEmail(String email) {
+        return getUserDtoByEmail(email);
+    }
+
+    private UserDto getUserDtoByEmail(String email) {
         return userMapper
-                .userToUserDto(userRepository.findByEmail(newUser.getEmail())
-                        .orElseThrow(() -> new EntityNotFoundException("Not found user with e-mail : " + newUser.getEmail())));
+                .userToUserDto(userRepository.findByEmail(email)
+                        .orElseThrow(() -> new EntityNotFoundException("Not found user with e-mail : " + email)));
     }
 }
